c5827ff1199499fecff73144496f9767e2448cba
Vandan17 <vandanp1705@gmail.com>
2026-07-23 19:58:58 +0530

    creating .env file and calling api

--- REDUX/redux-project/src/App.jsx
+++ REDUX/redux-project/src/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
-import Mediaapi from './api/mediaApi'
+import { fetchPhotos } from './api/mediaApi'
+import { fetchVideos } from './api/mediaApi'
+import { fetchGIF } from './api/mediaApi'
 
 const App = () => {
-  console.log(Mediaapi)
+  
   return (
     <div className="h-screen text-white w-full bg-gray-950">
-      Hi
+      <button 
+      onClick={async()=>{
+        const data = await fetchPhotos('cat')
+        console.log(data.results);
+      }}
+      >Get photos</button>
+
+      <button 
+      onClick={async()=>{
+        const data = await fetchVideos('dog')
+        console.log(data.videos);
+      }}
+      >Get Videos</button>
+
+      <button 
+      onClick={async()=>{
+        const data = await fetchGIF('dog')
+        console.log(data);
+      }}
+      >Get GIF</button>
     </div>
   )
 }
